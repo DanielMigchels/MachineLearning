c49c6d5bd6fb4ba6261b70865fa3f41ef8572173
Daniel Migchels <d.migchels@technology.asb.nl>
2020-06-09 13:51:35 +0200

    Add Experiment1

---
+++ ML.NET/Experiment1/MachineLearning/Program.cs
@@ -0,0 +1,21 @@
+﻿using MachineLearningML.Model;
+using System;
+
+namespace MachineLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var input = new ModelInput();
+                input.SentimentText = Console.ReadLine();
+                ModelOutput result = ConsumeModel.Predict(input);
+                Console.WriteLine("Toxic: " + result.Prediction);
+                Console.WriteLine();
+            }
+           
+        }
+    }
+}
